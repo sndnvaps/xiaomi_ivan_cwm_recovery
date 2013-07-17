1e2da45cf5ec5e9559db19dffd61291c87d82eea
PeterCxy <xqsx43cxy@126.com>
2013-07-18 07:20:16 +0800

    Add virtualkeys support for mione_plus and other 480p devices
    Change-Id: Ie25bdba4ef41d13b89d9c566fb4a4111660ca0f4

--- ui.c
+++ ui.c
@@ -70,6 +70,9 @@ static gr_surface *gInstallationOverlay;
 static gr_surface *gProgressBarIndeterminate;
 static gr_surface gProgressBarEmpty;
 static gr_surface gProgressBarFill;
+
+static gr_surface gVirtualKeys;
+
 static gr_surface gBackground;
 static int ui_has_initialized = 0;
 static int ui_log_stdout = 1;
@@ -85,6 +88,7 @@ static const struct { gr_surface* surface; const char *name; } BITMAPS[] = {
     { &gBackgroundIcon[BACKGROUND_ICON_FIRMWARE_ERROR], "icon_firmware_error" },
     { &gProgressBarEmpty,               "progress_empty" },
     { &gProgressBarFill,                "progress_fill" },
+    { &gVirtualKeys,                    "virtual_keys" },
     { &gBackground,                "stitch" },
     { NULL,                             NULL },
 };
@@ -226,6 +230,17 @@ static void draw_progress_locked()
     }
 }
 
+
+static void draw_virtualkeys_locked() {
+    gr_surface surface = gVirtualKeys;
+    int iconWidth = gr_get_width(surface);
+    int iconHeight = gr_get_height(surface);
+    int iconX = (gr_fb_width() - iconWidth) / 2;
+    int iconY = (gr_fb_height() - iconHeight);
+    gr_blit(surface, 0, 0, iconWidth, iconHeight, iconX, iconY);
+}
+
+
 static void draw_text_line(int row, const char* t) {
   if (t[0] != '\0') {
     gr_text(0, (row+1)*CHAR_HEIGHT-1, t);
@@ -254,9 +269,17 @@ static void draw_screen_locked(void)
         // gr_color(0, 0, 0, 160);
         // gr_fill(0, 0, gr_fb_width(), gr_fb_height());
 
-        int total_rows = gr_fb_height() / CHAR_HEIGHT;
+        gr_surface surface = gVirtualKeys;
+
+
+        int total_rows = (gr_fb_height() / CHAR_HEIGHT)
+            - (gr_get_height(surface) / CHAR_HEIGHT) - 1;
+
         int i = 0;
         int j = 0;
+
+        int offset = 0;     //offset of separating bar under menus
+
         int row = 0;            // current row that we are drawing on
         if (show_menu) {
 #ifndef BOARD_TOUCH_RECOVERY
@@ -293,7 +316,12 @@ static void draw_screen_locked(void)
             gr_fill(0, row*CHAR_HEIGHT+CHAR_HEIGHT/2-1,
                     gr_fb_width(), row*CHAR_HEIGHT+CHAR_HEIGHT/2+1);
 #else
-            row = draw_touch_menu(menu, menu_items, menu_top, menu_sel, menu_show_start);
+
+            if (menu_items <= max_menu_rows)
+                offset = 0;
+            gr_fill(0, (row - offset) * CHAR_HEIGHT + CHAR_HEIGHT / 2 - 1,
+                    gr_fb_width(), (row - offset) * CHAR_HEIGHT + CHAR_HEIGHT / 2 + 1);
+
 #endif
         }
 
@@ -311,6 +339,9 @@ static void draw_screen_locked(void)
             draw_text_line(start_row + r, text[(cur_row + r) % MAX_ROWS]);
         }
     }
+
+    draw_virtualkeys_locked();
+
 }
 
 // Redraw everything on the screen and flip the screen (make it visible).
@@ -388,11 +419,42 @@ static void *progress_thread(void *cookie)
 
 static int rel_sum = 0;
 
+static int in_touch = 0;        // 1 means in touch
+static int slide_right = 0;     //向右滑
+static int slide_left = 0;      //向左滑
+static int touch_x = 0;         //当前触摸的x坐标
+static int touch_y = 0;         //当前触摸的y坐标
+static int old_x = 0;           //之前触摸的x坐标
+static int old_y = 0;           //之前触摸的y坐标
+static int diff_x = 0;          //差异x坐标
+static int diff_y = 0;          //差异y坐标
+
+/**
+ *  对坐标记录数据进行复位操作
+ */
+static void reset_gestures() {
+    diff_x = 0;
+    diff_y = 0;
+    //old_x = 0;
+    //old_y = 0;
+    touch_x = 0;
+    touch_y = 0;
+}
+
+
 static int input_callback(int fd, short revents, void *data)
 {
     struct input_event ev;
     int ret;
     int fake_key = 0;
+
+    struct timeval curtime, newtime, oldtime;
+    int diff_sec;
+    gr_surface surface = gVirtualKeys;
+
+    int keywidth = gr_get_width(surface) / 4;
+    int keyoffset = (gr_fb_width() - gr_get_width(surface)) / 2;
+
 
     ret = ev_get_input(fd, revents, &ev);
     if (ret)
@@ -428,6 +490,87 @@ static int input_callback(int fd, short revents, void *data)
         }
     } else {
         rel_sum = 0;
+    }
+
+   int touch_type = EV_ABS;
+   int touch_code = 50;
+   int touch_pos_x = ABS_MT_POSITION_X;
+   int touch_pos_y = ABS_MT_POSITION_Y;
+    if (ev.type == touch_type && ev.code == touch_code && (touch_x != old_x || touch_y != old_y)) {
+        old_x = touch_x;
+        old_y = touch_y;
+        ev.type = EV_KEY;
+        if (touch_y > (gr_fb_height() - gr_get_height(surface))
+                && touch_y <= gr_fb_height() && touch_x > 0) {
+            //触摸区域在虚拟按键上
+            vibrate(10);    //振动
+            if (touch_x < (keywidth + keyoffset)) {
+                //最左的按键，下
+                ev.code = KEY_DOWN;
+                reset_gestures();
+            } else if (touch_x < ((keywidth * 2) + keyoffset)) {
+                //上
+                ev.code = KEY_UP;
+                reset_gestures();
+            } else if (touch_x < ((keywidth * 3) + keyoffset)) {
+                //返回
+                ev.code = KEY_BACK;
+                reset_gestures();
+            } else {
+                ev.code = KEY_POWER;    //KEY_ENTER
+                reset_gestures();
+            }
+        if (slide_right == 1) {
+            //右滑操作
+            slide_right = 0;
+        } else if (slide_left == 1) {
+            //左滑操作
+            slide_left = 0;
+        }
+
+        fake_key = 1;
+        ev.value = 1;
+        in_touch = 0;
+        reset_gestures();
+        }
+    }  else if (ev.type == touch_type && ev.code == touch_pos_x) {
+        touch_x = ev.value;
+        if (old_x != 0)
+            diff_x += touch_x - old_x;
+
+        if (touch_y < (gr_fb_height() - gr_get_height(surface))) {
+            if (diff_x > (gr_fb_width() / 4)) {
+                slide_right = 1;
+                reset_gestures();
+            } else if (diff_x < ((gr_fb_width() / 4) * -1)) {
+                slide_left = 1;
+                reset_gestures();
+            }
+        } else {
+            //input_buttons();
+            //reset_gestures();
+        }
+    } else if (ev.type == touch_type && ev.code == touch_pos_y) {
+        touch_y = ev.value;
+        if (old_y != 0)
+            diff_y += touch_y - old_y;
+
+        if (touch_y < (gr_fb_height() - gr_get_height(surface))) {
+            if (diff_y > ((gr_fb_height() - gr_get_height(surface)) / 4)) {
+                //下滑
+                //ev.code = KEY_DOWN;
+                //ev.type = EV_KEY;
+                //reset_gestures();
+            } else if (diff_y < ((gr_fb_height() - gr_get_height(surface)) / 4) * -1) {
+                //上滑
+                //ev.code = KEY_UP;
+                //ev.type = EV_KEY;
+                //reset_gestures();
+      }
+        } else {
+                //input_buttons();
+                //reset_gestures();
+        }
     }
 
     if (ev.type != EV_KEY || ev.code > KEY_MAX)
@@ -494,6 +637,9 @@ void ui_init(void)
     touch_init();
 #endif
 
+
+    gr_surface surface = gVirtualKeys;
+
     text_col = text_row = 0;
     text_rows = gr_fb_height() / CHAR_HEIGHT;
     max_menu_rows = text_rows - MIN_LOG_ROWS;
@@ -503,6 +649,9 @@ void ui_init(void)
     if (max_menu_rows > MENU_MAX_ROWS)
         max_menu_rows = MENU_MAX_ROWS;
     if (text_rows > MAX_ROWS) text_rows = MAX_ROWS;
+
+    text_rows = text_rows - (gr_get_height(surface) / CHAR_HEIGHT) -1;
+
     text_top = 1;
 
     text_cols = gr_fb_width() / CHAR_WIDTH;
@@ -579,6 +728,52 @@ void ui_init(void)
     pthread_t t;
     pthread_create(&t, NULL, progress_thread, NULL);
     pthread_create(&t, NULL, input_thread, NULL);
+}
+
+int input_buttons() {
+    int final_code = 0;
+    int start_draw = 0;
+    int end_draw = 0;
+    gr_surface surface = gVirtualKeys;
+
+    int keywidth = gr_get_width(surface) / 4;
+    int keyoffset = (gr_fb_width() - gr_get_width(surface)) / 2;
+    if (touch_x < (keywidth + keyoffset + 1)) {
+        final_code = KEY_DOWN;
+        start_draw = keyoffset;
+        end_draw = keywidth + keyoffset;
+    } else if (touch_x < ((keywidth * 2) + keyoffset + 1)) {
+        final_code = KEY_UP;
+        start_draw = keywidth + keyoffset + 1;
+        end_draw = (keywidth * 2) + keyoffset;
+    } else if (touch_x < ((keywidth * 3) + keyoffset + 1)) {
+        final_code = KEY_BACK;
+        start_draw = (keywidth * 2) + keyoffset + 1;
+        end_draw = (keywidth * 3) + keyoffset;
+    } else if (touch_x < ((keywidth * 4) + keyoffset + 1)) {
+        final_code = KEY_ENTER;
+        start_draw = (keywidth * 3) + keyoffset + 1;
+        end_draw = (keywidth * 4) + keyoffset;
+    }
+
+    if (touch_y > (gr_fb_height() - gr_get_height(surface)) && touch_x > 0) {
+        pthread_mutex_lock(&gUpdateMutex);
+        gr_color(0, 0, 0, 255);
+        gr_fill(0, gr_fb_height() - gr_get_height(surface) - 2,
+                start_draw - 1, gr_fb_height() - gr_get_height(surface));
+        gr_fill(end_draw + 1, gr_fb_height() - gr_get_height(surface) - 2,
+                gr_fb_width(), gr_fb_height() - gr_get_height(surface));
+        gr_color(MENU_TEXT_COLOR);
+        gr_fill(start_draw, gr_fb_height() - gr_get_height(surface) - 2,
+                end_draw, gr_fb_height - gr_get_height(surface));
+        gr_flip();
+        pthread_mutex_unlock(&gUpdateMutex);
+    }
+
+    if (in_touch == 1)
+        return final_code;
+    else
+        return 0;
 }
 
 char *ui_copy_image(int icon, int *width, int *height, int *bpp) {
@@ -1055,4 +1250,23 @@ void ui_delete_line() {
 void ui_increment_frame() {
     gInstallingFrame =
         (gInstallingFrame + 1) % ui_parameters.installing_frames;
+}
+
+int vibrate(int timeout_ms) {
+    char str[20];
+    int fd;
+    int ret;
+
+    fd = open("/sys/class/timed_output/vibrator/enable", O_WRONLY);
+    if (fd < 0)
+        return -1;
+
+    ret = snprintf(str, sizeof(str), "%d", timeout_ms);
+    ret = write(fd, str, ret);
+    close(fd);
+
+    if (ret < 0)
+       return -1;
+
+    return 0;
 }
